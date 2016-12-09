c6c3d962a6782c5c3dae1d8e535d11dde37d6035
SheilaRos <sheila.ros.cheme@gmail.com>
2016-12-09 17:50:35 +0100

    Acabada

--- src/main/java/com/mycompany/stucomroyal/StucomRoyalAPP.java
+++ src/main/java/com/mycompany/stucomroyal/StucomRoyalAPP.java
@@ -70,6 +70,7 @@ public class StucomRoyalAPP {
         String usuario = pedirCadenaNoVacia("Introduce el usuario:");
         String pass = pedirCadenaNoVacia("Introduce la contraseña:");
         Jugador validar = null;
+        //comprobamos si existe el usuario
         for(Jugador j : jugador){
             if(j.getUsuario().equals(usuario) && j.getPassword().equals(pass)){
                validar = j;
@@ -90,9 +91,11 @@ public class StucomRoyalAPP {
                 //muestra todas las cartas posibles y pide el nombre de la carta que quiere añadir
                mostrarCartas(cartas);
                String nombreCarta = pedirCadenaNoVacia("Introduce el nombre de la carta que quieres incluir en tus cartas:");
+               //Comprobamos si la carta existe y si no la tiene
                boolean comprobarCarta = comprobarCarta(j.getGrupoCartas(), nombreCarta);
                boolean comprobarExisteCarta = comprobarCarta(cartas, nombreCarta);
                if(!comprobarCarta && comprobarExisteCarta){
+                //Obtenemos carta y grabamos en el fichero
                 Cartas c = obtenerCarta(nombreCarta);
                 j.getGrupoCartas().add(c);
                 System.out.println("Carta añadida con exito.");
@@ -116,6 +119,7 @@ public class StucomRoyalAPP {
         return null;
     }
     public static boolean comprobarCarta(List<Cartas> lista, String carta2){
+        //comprobar si tiene o no la carta
         boolean laTiene=false;
         for(Cartas carta1 : lista){
             if(carta1.getNombre().equalsIgnoreCase(carta2)){
@@ -130,26 +134,41 @@ public class StucomRoyalAPP {
         }
     }
     public static void batalla(){
+        //creamos los array para clonar las cartas
         List<Cartas> cartasJ1 = new ArrayList<>();
         List<Cartas> cartasJ2 = new ArrayList<>();
+        //validamos los jugadores
         Jugador j1 = validar();
         Jugador j2 = validar();
+        //comprobamos que no se ha validado 2 veces el mismo jugador
         if(j1 == j2){
-            System.out.println("Se ha validado dos veces el mismo jugador.");
+            System.out.println("Se ha validado dos veces el mismo jugador o los usuarios no existen.");
         }else{
-            cartasJ1 = elegirCartas(j1, j1.getGrupoCartas());
-            cartasJ2 = elegirCartas(j2, j2.getGrupoCartas());
-            if(cartasJ1.size()>0 && cartasJ2.size()>0){
-            System.out.println("Cartas para la batalla de "+j1.getUsuario()+":");
-            System.out.println(cartasJ1);
-            System.out.println("Cartas para la batalla de "+j2.getUsuario()+":");
-            System.out.println(cartasJ2);
-            accionCartas(cartasJ1, cartasJ2, j1.getUsuario(), j2.getUsuario());
-            ganador(cartasJ1, cartasJ2, j1, j2);
+            //comprobamos que las cartas qur tiene el jugador son validas para una batalla
+            if((comprobarCosteElixir(j1.getGrupoCartas()) && j1.getGrupoCartas().size()==3) 
+                    || (comprobarCosteElixir(j2.getGrupoCartas()) && j2.getGrupoCartas().size()==3) 
+                    || j1.getGrupoCartas().size()<3 || j2.getGrupoCartas().size()<3){
+                System.out.println("Los jugadores o uno de los jugadores no tiene las cartas necesarias para poder combatir o tienen las justas y superan el coste de elixir.");
+            }else{
+                //Pedimos a los usuarios que seleccionen las cartas
+                cartasJ1 = elegirCartas(j1, j1.getGrupoCartas());
+                cartasJ2 = elegirCartas(j2, j2.getGrupoCartas());
+                //comprobamos que los array estan llenos
+                if(cartasJ1.size()>0 && cartasJ2.size()>0){
+                    System.out.println("Cartas para la batalla de "+j1.getUsuario()+":");
+                    System.out.println(cartasJ1);
+                    System.out.println("Cartas para la batalla de "+j2.getUsuario()+":");
+                    System.out.println(cartasJ2);
+                    //accion de las cartas
+                    accionCartas(cartasJ1, cartasJ2, j1.getUsuario(), j2.getUsuario());
+                    //comprobamos el ganador
+                    ganador(cartasJ1, cartasJ2, j1, j2);
+                }
             }
         }
     }
     public static void accionCartas(List<Cartas> cartas1, List<Cartas> cartas2, String nombre1, String nombre2){
+        //vemos quien comienza
         int random = (int)(Math.random()*2);
         if(random == 0){
             System.out.println("Comienza "+nombre1);
@@ -162,14 +181,18 @@ public class StucomRoyalAPP {
         }  
     }
     public static void ataca(List<Cartas> cartas1, List<Cartas> cartas2){
+        //acciones de las cartas
          for(int i=0; i<3; i++){
+             //comprobamos el tipo de carta
                 if(cartas1.get(i).getClass()== Tropa.class){
+                    //pasamos la carta al tipo que es
                     Tropa cartaT = (Tropa) cartas1.get(i);
                     int damage = cartaT.accionCarta();
                     damage = cartas2.get(i).getNivelVida() - damage;
                     cartas2.get(i).setNivelVida(damage);
                 }else if(cartas1.get(i).getClass()== Hechizo.class){
                    Hechizo cartaH = (Hechizo) cartas1.get(i);
+                   //comprobamos el modo de la carta
                    if(cartaH.getModo().equals("ataque")){
                        cartas2.get(i).setNivelVida(cartas2.get(i).getNivelVida() - cartaH.accionCarta());
                    }else{
@@ -184,10 +207,12 @@ public class StucomRoyalAPP {
     public static void ganador(List<Cartas> cartas1, List<Cartas> cartas2, Jugador j1, Jugador j2){
         int vidaJ1 = 0;
         int vidaJ2 = 0;
+        //suma la vida que queda
         for(int i = 0; i<3; i++){
             vidaJ1 += cartas1.get(i).getNivelVida();
             vidaJ2 += cartas2.get(i).getNivelVida();
         }
+        //Indica el ganador
         if(vidaJ1>vidaJ2){
             j1.setTrofeos(j1.getTrofeos()+5);
             System.out.println(j1.getUsuario()+" has ganado con una vida de "+vidaJ1+" a "+j2.getUsuario()+" con una vida de "+vidaJ2);
@@ -199,6 +224,7 @@ public class StucomRoyalAPP {
         }
     }
     public static boolean comprobarCosteElixir(List<Cartas> cartasJugador, Cartas cartita){
+        //compruebo coste de elixir, metodo para saber si las cartas clonadas y la nueva carta no superan los 10 de mana
         int elixir  = cartita.getCosteElixir();
         for(Cartas card : cartasJugador){
             elixir += card.getCosteElixir();
@@ -210,6 +236,7 @@ public class StucomRoyalAPP {
         }
     }
     public static boolean comprobarCosteElixir(List<Cartas> cartasJugador){
+        //compruebo coste de elixir, metodo para saber si las cartas que tiene el usuario no supera los 10 de mana
         int elixir=0;
         for(Cartas card : cartasJugador){
             elixir += card.getCosteElixir();
@@ -224,23 +251,26 @@ public class StucomRoyalAPP {
         //creo una lista para las cartas clonadas y un contador para el número máximo de cartas que puede seleccionar
         List<Cartas> cartaCopias = new ArrayList<>();
         int contador = 1;
-        if(comprobarCosteElixir(cartasJ) && cartasJ.size()<=3){
-            System.out.println("No se puede acceder a la batalla porque el jugador tiene 3 o menos cartas y la suma de los elixires supera 10");
-        }else{
         System.out.println(j.getUsuario()+" elige 3 cartas,en el orden que quieras usarlas, no puede superar 10 de elixir:");
         while(contador<4){
            boolean salir=false;
            do{ //enseño las cartas del jugador
             System.out.println(j.getGrupoCartas());
-            //Le digo que seleccione la carta por el nombre que tiene y compruebo que no la haya seleccionado ya
+            //Le digo que seleccione la carta por el nombre que tiene
             String nombre = pedirCadenaNoVacia("Selecciona el nombre de la "+contador+" carta que quieres elegir:");
-            boolean comprobarQueLaCartaEsteEnSuLista = comprobarCarta(j.getGrupoCartas(), nombre); 
+            //Compruebo que la carta está en su lista de cartas
+            boolean comprobarQueLaCartaEsteEnSuLista = comprobarCarta(j.getGrupoCartas(), nombre);
+            //compruebo que la carta seleccionada no la haya seleccionado ya
             boolean comprobarSiYaLaHaSeleccionado = comprobarCarta(cartaCopias, nombre);
+            //Si esta en la lista y no la ha seleccionado
             if(comprobarQueLaCartaEsteEnSuLista && !comprobarSiYaLaHaSeleccionado){
+                //obtengo la carta
                 Cartas cartita = obtenerCarta(nombre);
-                 boolean elixir = comprobarCosteElixir(cartaCopias, cartita);
+                //compruebo que el coste de elixir no se pase
+                boolean elixir = comprobarCosteElixir(cartaCopias, cartita);
                 if(!elixir){
                     try{
+                    //clono la carta
                     Cartas copia = (Cartas) cartita.clone();
                     cartaCopias.add(copia);
                     contador++;
@@ -251,25 +281,18 @@ public class StucomRoyalAPP {
                     System.out.println("Superas el coste de elixir, el máximo es 10.");
                 }
             }else{
-                System.out.println("La carta seleccionada no está en tu mazo o ya la has seleccionado.");
+                System.out.println("La carta seleccionada no está en tu mazo, no existe o ya la has seleccionado.");
             }
            }while(!salir);
         }
-       }
+        //devuelvo la lista de cartas clonadas
         return cartaCopias;
     }
     public static void ranking(){
-//        int max = 0;
-          int num = 1;
-//        for para conseguir el número de trofeos maximo
-//        for(Jugador j : jugador){
-//            if(j.getTrofeos()>max){
-//                max=j.getTrofeos();
-//            }
-//        }
+        int num = 1;
+        //con la clase Collection de java ordeno los jugadores por trofeo
         Collections.sort(jugador);
         System.out.println("<~~ Ranking ~~>");
-        //for para imprimir el ranking
         for(Jugador j: jugador){
                 System.out.println("");
                 System.out.println(num+" - "+ j.getUsuario()+", trofeos: "+j.getTrofeos());
